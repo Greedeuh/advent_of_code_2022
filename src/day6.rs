4e23338565bf223c1d3ae833635e875ec0fa6dcc
Greedeuh <adrien.olivon.dev@gmail.com>
2022-12-06 15:39:19 +0100

    Wouhou

--- src/day6.rs
+++ src/day6.rs
@@ -3,15 +3,17 @@ use itertools::Itertools;
 use std::fs;
 
 pub fn part1() {
-    let answer = find_marker(&input());
+    let answer = find_marker(&input(), 4);
     println!("day5, part1: {:?}", answer);
 }
 
 pub fn part2() {
-    println!("day5, part2: {:?}", "answer");
+    let answer = find_marker(&input(), 14);
+
+    println!("day5, part2: {:?}", answer);
 }
 
-fn find_marker(input: &str) -> (usize, String) {
+fn find_marker(input: &str, lenght: usize) -> (usize, String) {
     input
         .chars()
         .fold_while((0, "".to_string()), |(index, mut potential_marker), c| {
@@ -26,7 +28,7 @@ fn find_marker(input: &str) -> (usize, String) {
                 format!("{}{}", potential_marker, c)
             };
 
-            if potential_marker.len() == 4 {
+            if potential_marker.len() == lenght {
                 Done((index, potential_marker))
             } else {
                 Continue((index, potential_marker))
@@ -42,12 +44,13 @@ mod test {
 
     #[test]
     fn find_marker_bim() {
-        assert_eq!(find_marker("azer"), (4, "azer".to_string()));
-        assert_eq!(find_marker("aazer"), (5, "azer".to_string()));
-        assert_eq!(find_marker("azeazer"), (7, "azer".to_string()));
-        assert_eq!(find_marker("azeazerazer"), (7, "azer".to_string()));
-        assert_eq!(find_marker("azeazerqsdf"), (7, "azer".to_string()));
-        assert_eq!(find_marker("srlsrsnnwh"), (10, "nwh".to_string()));
+        assert_eq!(find_marker("azer", 4), (4, "azer".to_string()));
+        assert_eq!(find_marker("aazer", 4), (5, "azer".to_string()));
+        assert_eq!(find_marker("azeazer", 4), (7, "azer".to_string()));
+        assert_eq!(find_marker("azeazerazer", 4), (7, "azer".to_string()));
+        assert_eq!(find_marker("azeazerqsdf", 4), (7, "azer".to_string()));
+        assert_eq!(find_marker("srlsrsnnwh", 4), (10, "nwh".to_string()));
+        assert_eq!(find_marker("azer", 2), (2, "az".to_string()));
     }
 }
 
